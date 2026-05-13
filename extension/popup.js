4f360a0fe9831f81342df0ff09ad3f91d597eada
Viktor Stolenets <vs.00000000000000000@gmail.com>
2026-05-14 01:07:56 +0300

    add solana

--- extension/popup.js
+++ extension/popup.js
@@ -1,15 +1,60 @@
-import init, { eth_generate } from "/pkg/ex_chrome.js";
+import init, { eth_generate, solana_generate } from "./pkg/ex_chrome.js";
 const wasmPromise = init();
 
-async function runWasm() {
-  await wasmPromise;
-  const result = eth_generate();
-  document.getElementById("status").innerHTML = `
-    <p><b>Etherum</b></p>
-    <hr/>
-    <p><b>Private Key:</b> ${result.private_key}</p>
-    <p><b>Address:</b> ${result.address}</p>
-  `;
+document.addEventListener("DOMContentLoaded", async () => {
+  const data = await chrome.storage.local.get({ walletHistory: "" });
+  document.getElementById("content").innerHTML = data.walletHistory;
+});
+
+async function saveToStorage() {
+  const currentHTML = document.getElementById("content").innerHTML;
+  await chrome.storage.local.set({ walletHistory: currentHTML });
 }
 
-document.getElementById("runEtherum").addEventListener("click", runWasm);
+async function runClean() {
+  document.getElementById("content").innerHTML = "";
+  await saveToStorage();
+}
+
+async function runEth() {
+  await wasmPromise;
+  const result = eth_generate();
+
+  const htmlContent = `
+    <div class="wallet-entry">
+      <p><b>Ethereum</b></p>
+      <p><b>Private Key:</b> ${result.private_key}</p>
+      <p><b>Address:</b> ${result.address}</p>
+      <hr>
+    </div>
+  `;
+
+  document
+    .getElementById("content")
+    .insertAdjacentHTML("beforeend", htmlContent);
+
+  await saveToStorage();
+}
+
+async function runSolana() {
+  await wasmPromise;
+  const result = solana_generate();
+
+  const htmlContent = `
+    <div class="wallet-entry">
+      <p><b>Solana</b></p>
+      <p><b>Private Key:</b> ${result.private_key}</p>
+      <p><b>Address:</b> ${result.address}</p>
+      <hr>
+    </div>
+  `;
+
+  document
+    .getElementById("content")
+    .insertAdjacentHTML("beforeend", htmlContent);
+  await saveToStorage();
+}
+
+document.getElementById("runEtherum").addEventListener("click", runEth);
+document.getElementById("runSolana").addEventListener("click", runSolana);
+document.getElementById("runClean").addEventListener("click", runClean);
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use ethers::core::rand::thread_rng;
 use ethers::signers::{LocalWallet, Signer};
 use serde::Serialize;
+use solana_sdk::bs58;
+use solana_sdk::signer::Signer as SignerSolana;
 use wasm_bindgen::prelude::*;
 
 #[derive(Serialize)]
@@ -14,12 +16,25 @@ pub fn eth_generate() -> JsValue {
     let wallet = LocalWallet::new(&mut thread_rng());
 
     let private_key_hex = hex::encode(wallet.signer().to_bytes());
+    let private_key = format!("0x{}", private_key_hex);
     let address = format!("{:?}", wallet.address());
 
     let pair = Pair {
-        private_key: format!("0x{}", private_key_hex),
+        private_key,
         address,
     };
 
+    serde_wasm_bindgen::to_value(&pair).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn solana_generate() -> JsValue {
+    let wallet = solana_sdk::signer::keypair::Keypair::new();
+    let private_key = bs58::encode(wallet.to_bytes()).into_string();
+    let address = wallet.pubkey().to_string();
+    let pair = Pair {
+        private_key,
+        address,
+    };
     serde_wasm_bindgen::to_value(&pair).unwrap()
 }
